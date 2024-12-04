b92f4bfd9bfd4e7c7f0cc308fcd78443763f8d94
Lukhanyo Radebe <khanyoradebe46@gmail.com>
2024-12-04 18:52:26 +0200

    implement sub functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from http import server
 import subprocess
 import wolframalpha
 import pyttsx3
@@ -28,3 +29,66 @@ from urllib.request import urlopen
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning Sir!")
+
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon Sir!")
+
+    else:
+        speak("Good Evening Sir!")
+
+    assname = ("Jarvis 1 point o")
+    speak("I am your Assistant")
+    speak(assname)
+
+
+def takeCommand():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language = 'en-us')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print(e)
+        print("Unable to recognize your voice")
+        return "None"
+
+    return query
+
+def username():
+    speak("What should i call you sir?")
+    uname = takeCommand()
+    speak("Welcome ")
+    speak(uname)
+    columns = shutil.get_terminal_size().columns
+
+    print("####################".center(columns))
+    print("Welcome Mr.", uname.center(columns))
+    print("####################".center(columns))
+
+    speak("How can i help you, Sir?")
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+
+    # Enable low security in gmail
+    server.login('your email id', 'your email password')
+    server.sendmail('your email id', to, content)
+    server.close()
